e837c59ff5acde446c9ed67d618a7f26320eb34b
Suhas Pradhan <spradhan@qwinix.io>
2016-09-29 18:23:28 +0530

    29th_sept

--- BackEnd/Ruby/fileIO.rb
+++ BackEnd/Ruby/fileIO.rb
@@ -15,8 +15,32 @@ file=File.new("sysread.txt","r+")
 #  file.syswrite("Hey")
 # a = file.sysread(5)
 # puts a
-puts file.readlines("sysread.txt")
+# puts file.readlines("sysread.txt")
           #OR
-puts IO.readlines("sysread.txt")
+# puts IO.readlines("sysread.txt")
+puts "Does the file exist?"
+file=File.exist?("sysread.txt")
+puts file
+puts "What is the size of the file?"
+file=File.size("sysread.txt")
+puts file
+puts "Is it readable? writable? executable?"
+puts File.readable?( "sysread.txt" )   
+puts File.writable?( "sysread.txt" )   
+puts File.executable?( "sysread.txt" )
+
+puts "print true if file exists and open it"
+puts "true" if File.open("sample.txt")if File.exist?("sampl.txt")
+
+# puts "To rename the file"
+# File.rename("sample.txt","sample.txt")
+
+puts "To delete a file"
+begin
+	File.delete("sample.txt")
+	rescue Exception => e
+	puts e.message
+end
+
 
 
