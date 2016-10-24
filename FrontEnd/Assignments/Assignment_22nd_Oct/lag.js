7016d6ae239e0c93a62f7865fb9f2012d07302e6
Suhas Pradhan <spradhan@qwinix.io>
2016-10-24 15:29:41 +0530

    Assignment_22nd_Oct

---
+++ FrontEnd/Assignments/Assignment_22nd_Oct/lag.js
@@ -0,0 +1,10 @@
+
+  function dispHome()
+  {
+  document.getElementById('h').style.display='block';
+  document.getElementById('a').style.display='none';
+  }
+  function dispAbout(){
+  document.getElementById('a').style.display='block';
+  document.getElementById('h').style.display='none';
+  }
